1e71ba4db95380c394282a8418276661f995c64d
Mohammad  Rajaei <mrajaeim.pro@gmail.com>
2025-03-09 23:23:14 +0330

    feat: init variation repository

--- modules/datastore/__init__.py
+++ modules/datastore/__init__.py
@@ -1,9 +1,11 @@
 from .product_repository import ProductRepository
 from .image_repository import ImageRepository
 from .failed_crawl_repository import FailedCrawlRepository
+from .variation_repository import VariationRepository
 
 db_path = "./data/crawler.db"
 
 product_repo = ProductRepository(db_path)
 image_repo = ImageRepository(db_path)
 failed_crawl_repo = FailedCrawlRepository(db_path)
+variation_repo = VariationRepository("data/crawler.db")
